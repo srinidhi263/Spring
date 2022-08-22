daae29a2e28e98a5db8de191869e76854ba8304d
GeeksforGeeks <srinidhimukunthan@gmail.orgg>
2022-08-22 17:52:57 +0530

    INITIAL COMMIT

--- src/main/java/com/nseit/spring/Application.java
+++ src/main/java/com/nseit/spring/Application.java
@@ -9,7 +9,15 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
         BinarySearchImpl binarySearch = applicationContext.getBean(BinarySearchImpl.class);
+        BinarySearchImpl binarySearch2 = applicationContext.getBean(BinarySearchImpl.class);
+
+        System.out.println(binarySearch);
+        System.out.println(binarySearch2);
+
         int result = binarySearch.binarySearch(new int[]{3, 7, 9, 12}, 7);
+
+
+
         System.out.println(result);
         //SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/nseit/spring/BinarySearchImpl.java
+++ src/main/java/com/nseit/spring/BinarySearchImpl.java
@@ -1,9 +1,13 @@
 package com.nseit.spring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BinarySearchImpl {
 
 
